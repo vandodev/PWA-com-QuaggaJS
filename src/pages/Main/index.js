4074609d432d7e0b1da34ecfbcf366be8fc6c29e
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-14 00:03:45 -0300

    :fire: Transformando Scanner em component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,65 +1,8 @@
-import React, { useEffect } from 'react';
-import Quagga from 'quagga';
-import { Video } from './styles';
-import { validateIsbn } from '../../services/books';
+import React from 'react';
+import Scanner from './Scanner';
 
 function Main() {
-  let scannerAttemps = 0;
-  const onDetected = (result) => {
-    Quagga.offDetected(onDetected);
-    const isbn = result.codeResult.code;
-    console.log(`Código lido: ${isbn}`);
-    if (validateIsbn(isbn)) {
-      alert(` ISBN válido ${isbn}`);
-      return;
-    } else {
-      if (scannerAttemps >= 5) {
-        alert(
-          'Não é possível ler o código do livro, por favor, tente novamente.'
-        );
-      }
-    }
-
-    scannerAttemps++;
-    Quagga.onDetected(onDetected);
-  };
-
-  useEffect(() => {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      Quagga.init(
-        {
-          inputStream: {
-            name: 'Live',
-            type: 'LiveStream',
-            target: document.querySelector('#Video'), // Or '#yourElement' (optional)
-            constraints: {
-              facingMode: 'environment',
-            },
-          },
-          numOfWorkers: 1,
-          locate: true,
-          decoder: {
-            // readers: ['code_128_reader'],
-            readers: ['ean_reader'],
-          },
-        },
-        (err) => {
-          if (err) {
-            console.log(err);
-            alert(
-              'Erro ao abrir a camera do dispositivo, por favor, dê a autorização de uso'
-            );
-            return;
-          }
-          console.log('Initialization finished. Ready to start');
-          Quagga.start();
-        },
-        Quagga.onDetected(onDetected)
-      );
-    }
-  }, []);
-
-  return <Video id="Video" />;
+  return <Scanner />;
 }
 
 export default Main;
